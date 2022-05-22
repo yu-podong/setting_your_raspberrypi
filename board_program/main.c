521d0b7e9c76d39cddab74d96e54927ad14297e8
seungyeon <57320841+ksygt728@users.noreply.github.com>
2022-05-22 19:59:07 +0900

    pump.c upload

---
+++ board_program/main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <signal.h> //Signal 사용 헤더파일
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h> //exit() 사용 헤더파일
+
+#include <wiringPi.h>
+
+#define PUMP 21 // BCM_GPIO 5
+
+void func_pumpon();
+void sig_handler(int signo); // 마지막 종료 함수
+int main(void)
+{
+  func_pumpon();
+  return 0;
+}
+
+/*** 워터펌프 동작 ***/
+void func_pumpon()
+{
+  signal(SIGINT, (void *)sig_handler); //시그널 핸들러 함수
+
+  if (wiringPiSetup() == -1)
+  {
+    fprintf(stdout, "Unable to start wiringPi: %s\n", strerror(errno));
+    return 1;
+  }
+
+  pinMode(PUMP, OUTPUT);
+
+  for (;;)
+  {
+    printf("here - pump on\n");
+    digitalWrite(PUMP, 1); // On
+
+    delay(2000); // ms
+
+    digitalWrite(PUMP, 0); // Off
+
+    delay(2000);
+  }
+}
+void sig_handler(int signo)
+{
+  printf("process stop\n");
+  digitalWrite(PUMP, 0); // Off
+
+  exit(0);
+}
